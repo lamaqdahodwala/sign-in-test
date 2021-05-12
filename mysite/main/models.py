09d7479738eecd2edd2496b088f3ce41eb02eb29
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-12 11:55:50 -0400

    i know what to do with this random git repo now

--- mysite/main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    author = models.CharField(max_length=100)
-    title = models.CharField(max_length=300)
-    body = models.TextField()
