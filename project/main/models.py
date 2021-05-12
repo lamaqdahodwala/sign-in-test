526109bd0b0170ad078de0d0282440d2fb70dbed
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-12 10:38:40 -0400

    created models, views, html

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
 # Create your models here.
+class User(AbstractUser):
+    ... 
+
+class Post(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
